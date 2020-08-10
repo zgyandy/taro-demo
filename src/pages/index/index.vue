db23966c32b9023ba07f97c66be49df4821ba1ab
zgy <guiyu.zhang@haoyishu.org>
2020-08-10 16:31:56 +0800

    firstcommit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.index {
+  width: 7.5rem;
+}
+</style>
